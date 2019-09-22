eb2871fde49925ab07816477c51940dc46082e68
Lyulyaev Maxim <lyulyaev.maxim@gmail.com>
2019-09-22 03:28:47 +0300

    fixed merge conflicts

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,33 +3,35 @@ import i18next from 'i18next'
 import 'modules/i18'
 import { ThemeProvider } from '@material-ui/styles'
 // import { StyleSheetManager } from 'styled-components'
-// import { createGlobalStyle } from 'styled-components'
-import { CssBaseline } from '@material-ui/core';
+import { createGlobalStyle } from 'styled-components'
+import { CssBaseline as MaterialCSSReset } from '@material-ui/core'
 import { theme } from 'styles/theme'
 import { SEO } from 'components/seo'
 import { Notifications } from 'components/notifications'
+import { Header } from 'components/header'
+// import './index.css'
 
 /*const styledComponentsPlugins = [
   stylisRTLPlugin
 ]*/
 
-/*const GlobalStyle = createGlobalStyle`
-  body {
-    color: red;
+const CustomCSSReset = createGlobalStyle`
+  a {
+    color: inherit;
+    text-decoration: none;
   }
-`*/
+`
 
 export const wrapPageElement = ({ element, props }) => {
   const { pageContext } = props
   i18next.changeLanguage(pageContext.locale)
-  console.log('theme: ', theme)
   return (
     <>
-      <CssBaseline />
+      <MaterialCSSReset />
+      <CustomCSSReset />
       <SEO locale={pageContext.locale} pageName={pageContext.pageName} />
       {/*<StyleSheetManager stylisPlugins={styledComponentsPlugins}>*/}
       <ThemeProvider theme={theme}>
-        {/*<GlobalStyle />*/}
         {element}
         <Notifications />
       </ThemeProvider>
@@ -37,3 +39,20 @@ export const wrapPageElement = ({ element, props }) => {
     </>
   )
 }
+
+interface ILayout {
+  children: React.ReactNode
+}
+
+export const Layout : React.FC<ILayout> = props => (
+  <>
+    <Header />
+    <main>{props.children}</main>
+    <footer>
+      {`© ${new Date().getFullYear()}, Built with  `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </footer>
+  </>
+)
+
+
